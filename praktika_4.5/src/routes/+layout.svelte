8ca8d291ba803d116182c1c29464cac038675782
Elena <rbelena@list.ru>
2024-04-13 22:44:13 +0400

    adding files

---
+++ praktika_4.5/src/routes/+layout.svelte
@@ -0,0 +1,40 @@
+<script>
+    const nav = [
+    {
+      title: 'Портфолио',
+      href: '/first',
+	  isShow: true,
+
+    },
+    ]
+</script>
+
+
+<style>
+	.container {
+	  max-width: 1400px;
+	  margin: 50px auto;
+	}
+
+</style>
+
+<nav>
+	<div class="nav-container">
+	  <a href="/" class="nav-logo" title="На главную страницу">На главную страницу</a>
+	  <div class="nav-links">
+		{#each nav as link}
+			{#if link.isShow}
+			<a href={link.href} class="link">{link.title}</a>
+			{/if}
+		{/each}
+	  </div>
+	</div>
+  </nav>
+
+  <div class="container">
+	<!-- Pages will be injected below -->
+	<slot></slot>
+  </div> 
+  
+  
+  
---
+++ praktika_4.5/src/routes/first/+page.svelte
@@ -0,0 +1,48 @@
+<svelte:head>
+ <title>Первый проект - портфолио</title>
+</svelte:head>
+
+<div class='container'>
+ <h1>Мои проекты</h1>
+ <div class='apps'>
+  {#each routes as page}
+   <div class='app'>
+    <h2>{page.title}</h2>
+    <p>{page.body}</p>
+    <p class='readmore'>
+     <a class='link' href={`/first/apps/${page.id}`}>
+      Читать далее
+     </a>
+    </p>
+   </div>
+  {/each}
+ </div>
+</div>
+
+<script>
+    import { routes } from './routes.js';
+</script>
+
+<style>
+    .container {
+     margin: 50px auto;
+     max-width: 800px;
+     padding: 0 20px;
+    }
+  
+    .apps {
+     display: grid;
+     grid-template-columns: 1fr 1fr;
+     grid-gap: 20px;
+    }
+    .app {
+   border: 1px solid #6b2088;
+   padding: 10px;
+   box-shadow: 0 0 10px #640000;
+  }
+
+  .link {
+   color: rgb(8, 8, 99);
+  }
+ </style>
+  
